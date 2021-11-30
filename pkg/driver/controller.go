f1428b374db234e3935ff8484348b1eede1e937f
Florian Walter <fw@digio.ch>
2021-11-30 17:38:51 +0100

    fixed merge request feedback

--- pkg/driver/controller.go
+++ pkg/driver/controller.go
@@ -2,6 +2,7 @@ package driver
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math"
 	"net/http"
@@ -17,8 +18,6 @@ import (
 	"k8s.io/klog"
 
 	"github.com/flowswiss/goclient/flow"
-
-	"github.com/flowswiss/csi-driver/pkg/util"
 )
 
 const (
@@ -613,7 +612,7 @@ func (d *Driver) CreateSnapshot(ctx context.Context, request *csi.CreateSnapshot
 					SnapshotId:     snapshot.Id.String(),
 					SourceVolumeId: snapshot.Volume.Id.String(),
 					CreationTime:   timestamp,
-					ReadyToUse:     util.MapSnapshotReadyToUse(snapshot),
+					ReadyToUse:     snapshot.IsAvailable(),
 				},
 			}, nil
 		}
@@ -639,19 +638,21 @@ func (d *Driver) CreateSnapshot(ctx context.Context, request *csi.CreateSnapshot
 		Factor:   1.25,
 		Steps:    7,
 	}
+
 	err = wait.ExponentialBackoff(backoff, func() (done bool, err error) {
 		temp, _, err := d.flow.Snapshot.Get(ctx, snapshot.Id)
 		if err != nil {
 			// ignore the error and keep trying if the request fails
-			klog.Info(fmt.Sprintf("Failed to fetch snapshot status for snapshot with id %d", temp.Id))
+			klog.Info(fmt.Sprintf("Failed to fetch snapshot status for snapshot with id %d", snapshot.Id))
 			return false, nil
 		}
-		available, err := temp.IsAvailable()
-		if err != nil {
+
+		if !temp.IsAvailable() {
 			klog.Info(fmt.Sprintf("Waiting for Snapshot with id %d to be available, currently in %s state", temp.Id, temp.Status.Key))
-			return false, err
+			return false, nil
 		}
-		return available, nil
+
+		return true, nil
 	})
 
 	timestamp, err := ptypes.TimestampProto(snapshot.CreatedAt.Time())
@@ -659,7 +660,7 @@ func (d *Driver) CreateSnapshot(ctx context.Context, request *csi.CreateSnapshot
 		return nil, status.Error(codes.Internal, err.Error())
 	}
 
-	if err == wait.ErrWaitTimeout {
+	if errors.Is(err, wait.ErrWaitTimeout) {
 		klog.Info("Timeout exceeded, snapshot is still creating. CSI Will respond with ReadyToUse set to false")
 		return &csi.CreateSnapshotResponse{
 			Snapshot: &csi.Snapshot{
@@ -782,7 +783,7 @@ func (d *Driver) ListSnapshots(ctx context.Context, request *csi.ListSnapshotsRe
 				SnapshotId:     snapshot.Id.String(),
 				SourceVolumeId: snapshot.Volume.Id.String(),
 				CreationTime:   timestamp,
-				ReadyToUse:     util.MapSnapshotReadyToUse(snapshot),
+				ReadyToUse:     snapshot.IsAvailable(),
 			},
 		})
 	}
